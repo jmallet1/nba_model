5df384b3fbc5ee0b764af6a63bc5ec3bfeadf49b
jmallet1 <99818382+jmallet1@users.noreply.github.com>
2024-02-22 18:18:48 -0600

    Adding files.

---
+++ PRIZEPICKS_NBA_SCRAPER.py
@@ -0,0 +1,81 @@
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import pyautogui as p
+import time
+import pandas as pd
+
+'''
+Change self.directory to the folder where typical downloads go to. end the directory in projections.json
+github API: https://github.com/auroradan/PrizePicks-Prop-Finder/blob/main/README.md00
+'''
+
+class PRIZEPICKS_NBA_SCRAPER():
+    def __init__(self):
+        self.directory = "C:\\Users\\jakem\\Downloads\\projections.json"
+        self.lines = []
+        self.getJSON()
+        self.load()
+        df = pd.DataFrame(self.lines, columns=['Name','Type','Line'])
+        df.to_csv('lines.csv', index=False)
+
+    def getJSON(self):
+        url = "https://api.prizepicks.com/projections?league_id=7"
+        driver = webdriver.Firefox()
+        driver.get(url)
+        wait = WebDriverWait(driver, 10)
+        wait.until(EC.presence_of_element_located((By.ID, "json-tab")))
+        save_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn.save")))
+        save_button.click()
+        time.sleep(2)
+        p.press("enter")
+        time.sleep(.5)
+        p.press("left")
+        time.sleep(.5)
+        p.press("enter")
+        time.sleep(2)
+        driver.quit()
+
+    def load(self):
+        seive = {"points", "rebounds", "assists", "threes", "blocks", "steals", "pra", "pr", "pa", "ra"}
+        with open(self.directory, 'r') as file:
+            json_data = json.load(file)
+        player_names = {elem["id"]: elem["attributes"]["name"]
+                        for elem in json_data["included"]
+                        if elem["type"] == "new_player"}
+        player_projections = []
+        for projection in json_data["data"]:
+            if projection["type"] == "projection":
+
+                player_id = projection["relationships"]["new_player"]["data"]["id"]
+                player_name = player_names.get(player_id, "Unknown Player")
+
+                flash_sale = projection["attributes"].get("flash_sale_line_score")
+                line_score = projection["attributes"]["line_score"]
+                stat_type = self.statType(projection["attributes"]["stat_type"]).lower()
+                if stat_type in seive and projection["attributes"].get("adjusted_odds") is not True:
+                    player_projections.append((player_name, stat_type, line_score))
+                if stat_type in seive and flash_sale is not None:
+                    player_projections.append((player_name, stat_type, flash_sale))
+        self.lines = player_projections
+
+    def statType(self, stat):
+        match stat:
+            case "Pts+Rebs+Asts":
+                return "pra"
+            case "Pts+Asts":
+                return "pa"
+            case "Pts+Rebs":
+                return "pr"
+            case "Rebs+Asts":
+                return "ra"
+            case "3-PT Made":
+                return "threes"
+            case "Blocked Shots":
+                return "blocks"
+            case _:
+                return stat
+
+PRIZEPICKS_NBA_SCRAPER()
---
+++ stat_scrape_v2.py
@@ -0,0 +1,174 @@
+import datetime
+import time
+
+from nba_api.stats.endpoints import leagueleaders
+from nba_api.stats.endpoints import playergamelogs
+from nba_api.stats.endpoints import teamestimatedmetrics
+from nba_api.stats.endpoints import playerestimatedmetrics
+from nba_api.stats.endpoints import leaguedashteamstats
+from nba_api.stats.static import teams
+import pandas as pd
+
+
+def pace_mapping(row):
+    team_id = row['TEAM_ID']
+    for team in team_metrics:
+        if team[1] == team_id:
+            return team[10]
+
+
+def offrtg_mapping(row):
+    team_id = row['TEAM_ID']
+    for team in team_metrics:
+        if team[1] == team_id:
+            return team[7]
+
+
+def usgrtg_mapping(row):
+    pid = row['PLAYER_ID']
+    for player in player_metrics:
+        if player[0] == pid:
+            return player[15]
+
+
+def defrtg_mapping(row):
+    # substr opp team abbrev
+    opp_team_abbrev = row['MATCHUP'][-3:]
+
+    # translate team abrev to team id
+    opp_team = teams.find_team_by_abbreviation(opp_team_abbrev)
+    opp_team_id = opp_team.get('id')
+
+    # select the row with the right team id, then get the value of the defensive rating
+    return team_stats.loc[team_stats['TEAM_ID'] == opp_team_id]['DEF_RATING'].values[0]
+
+
+def sc_mapping(row):
+    # substr opp team abbrev
+    opp_team_abbrev = row['MATCHUP'][-3:]
+
+    # translate team abrev to team id
+    opp_team = teams.find_team_by_abbreviation(opp_team_abbrev)
+    opp_team_id = opp_team.get('id')
+
+    # select the row with the right team id, then get the value of the defensive rating
+    return team_stats.loc[team_stats['TEAM_ID'] == opp_team_id]['OPP_PTS_2ND_CHANCE'].values[0]
+
+
+def fbp_mapping(row):
+    # substr opp team abbrev
+    opp_team_abbrev = row['MATCHUP'][-3:]
+
+    # translate team abrev to team id
+    opp_team = teams.find_team_by_abbreviation(opp_team_abbrev)
+    opp_team_id = opp_team.get('id')
+
+    # select the row with the right team id, then get the value of the defensive rating
+    return team_stats.loc[team_stats['TEAM_ID'] == opp_team_id]['OPP_PTS_FB'].values[0]
+
+
+def pinp_mapping(row):
+    # substr opp team abbrev
+    opp_team_abbrev = row['MATCHUP'][-3:]
+
+    # translate team abrev to team id
+    opp_team = teams.find_team_by_abbreviation(opp_team_abbrev)
+    opp_team_id = opp_team.get('id')
+
+    # select the row with the right team id, then get the value of the defensive rating
+    return team_stats.loc[team_stats['TEAM_ID'] == opp_team_id]['OPP_PTS_PAINT'].values[0]
+
+
+master_df = pd.DataFrame()
+seasons = ['2014-15', '2015-16', '2016-17', '2017-18', '2018-19', '2019-20', '2020-21', '2021-22', '2022-23']
+
+for season in seasons:
+    print("On season: ", season)
+    print("Current Time: ", datetime.datetime.now())
+
+    # Pull data for the top 100 scorers
+    top_200 = leagueleaders.LeagueLeaders(
+        season=season,
+        season_type_all_star='Regular Season',
+        stat_category_abbreviation='PTS'
+    ).get_data_frames()[0][:150]
+
+    team_stats = leaguedashteamstats.LeagueDashTeamStats(
+        season=season,
+        per_mode_detailed='PerGame',
+        measure_type_detailed_defense='Defense',
+        season_type_all_star='Regular Season'
+    ).get_data_frames()[0]
+
+    team_metrics = teamestimatedmetrics.TeamEstimatedMetrics(
+        season=season,
+        season_type='Regular Season'
+    ).get_dict().get('resultSet')['rowSet']
+
+    player_metrics = playerestimatedmetrics.PlayerEstimatedMetrics(
+        season=season,
+        season_type='Regular Season'
+    ).get_dict().get('resultSet')['rowSet']
+
+    # Inspect the first few rows of the averaged stats
+    player_ids = top_200.PLAYER_ID.values.tolist()
+
+    for player_id in player_ids:
+
+        time.sleep(5)
+        game_log = playergamelogs.PlayerGameLogs(
+            player_id_nullable=player_id,
+            season_type_nullable='Regular Season',
+            season_nullable=season).get_data_frames()[0]
+
+        game_log['SEASON AVG FG_PCT'] = game_log['FG_PCT'].mean()
+        game_log['SEASON AVG FG3_PCT'] = game_log['FG3_PCT'].mean()
+        game_log['SEASON AVG FT_PCT'] = game_log['FT_PCT'].mean()
+        game_log['SEASON AVG OREB'] = game_log['OREB'].mean()
+        game_log['SEASON AVG REB'] = game_log['REB'].mean()
+        game_log['SEASON AVG AST'] = game_log['AST'].mean()
+        game_log['SEASON AVG TOV'] = game_log['TOV'].mean()
+        game_log['SEASON AVG STL'] = game_log['STL'].mean()
+        game_log['SEASON AVG PF'] = game_log['PF'].mean()
+        game_log['SEASON AVG PFD'] = game_log['PFD'].mean()
+        game_log['SEASON AVG PLUS_MINUS'] = game_log['PLUS_MINUS'].mean()
+
+        game_log['5 GM PTS'] = game_log['PTS'].rolling(window=5, min_periods=1, closed='left').mean()
+        game_log['10 GM PTS'] = game_log['PTS'].rolling(window=10, min_periods=1, closed='left').mean()
+        game_log['SEASON AVG PTS'] = game_log['PTS'].mean()
+
+        game_log['5 GM MIN'] = game_log['MIN'].rolling(window=5, min_periods=1, closed='left').mean()
+        game_log['10 GM MIN'] = game_log['MIN'].rolling(window=10, min_periods=1, closed='left').mean()
+        game_log['SEASON AVG MIN'] = game_log['MIN'].mean()
+
+        game_log['USG'] = game_log.apply(usgrtg_mapping, axis=1)
+        game_log['PACE'] = game_log.apply(pace_mapping, axis=1)
+        game_log['TEAM OFF RTG'] = game_log.apply(offrtg_mapping, axis=1)
+        game_log['OPP DEF RTG'] = game_log.apply(defrtg_mapping, axis=1)
+        game_log['OPP 2nd CHANCE'] = game_log.apply(sc_mapping, axis=1)
+        game_log['OPP FB POINTS'] = game_log.apply(fbp_mapping, axis=1)
+        game_log['OPP PinP'] = game_log.apply(pinp_mapping, axis=1)
+
+        # should these be season averages? simple .mean() on game log would do that
+        game_log = game_log.assign(TS=lambda x: x['PTS'] / (2 * (x['FGA'] + (0.44 * x['FTA']))))
+        game_log['SEASON AVG TS'] = game_log['TS'].mean()
+
+        game_log = game_log.assign(eFG=lambda x: (x['FGM'] + 0.5 * x['FG3M']) / x['FGA'])
+        game_log['SEASON AVG EFG'] = game_log['eFG'].mean()
+
+
+        game_log['SEASON AVG PFD'] = game_log['PFD'].mean()
+        game_log['SEASON AVG PLUS_MINUS'] = game_log['PLUS_MINUS'].mean()
+
+        game_log = game_log[
+            ['PTS', 'SEASON AVG FG_PCT', 'SEASON AVG FG3_PCT', 'SEASON AVG FT_PCT', 'SEASON AVG OREB',
+             'SEASON AVG REB', 'SEASON AVG AST', 'SEASON AVG TOV', 'SEASON AVG STL', 'SEASON AVG PF', 'SEASON AVG PFD',
+             'SEASON AVG PLUS_MINUS', '5 GM PTS', '10 GM PTS', 'SEASON AVG PTS', '5 GM MIN', '10 GM MIN',
+             'SEASON AVG MIN', 'USG', 'PACE', 'TEAM OFF RTG',
+             'OPP DEF RTG', 'OPP 2nd CHANCE', 'OPP FB POINTS', 'OPP PinP',
+             'SEASON AVG TS', 'SEASON AVG EFG'
+             ]]
+
+        master_df = pd.concat([master_df, game_log], axis=0)
+
+master_df.to_csv('player_data.csv')
